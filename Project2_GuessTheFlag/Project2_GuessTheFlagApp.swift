fb9a171e287dfb359f385c2809af5d3612279b28
BM Kane <kane@brickmate.vn>
2024-01-02 13:40:52 +0700

    Initial Commit

---
+++ Project2_GuessTheFlag/Project2_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project2_GuessTheFlagApp.swift
+//  Project2_GuessTheFlag
+//
+//  Created by Kane on 02/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Project2_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
